ae7d4cb3c310ee40300148b56cea044793cefc92
Clu-Codes <retsulcconnor@gmail.com>
2023-11-06 07:44:15 -0500

    init commit

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+book_path = "./books/frankenstein.txt"
+
+
 def main():
-    book_path = "./books/frankenstein.txt"
     text = open_books(book_path)
     count = num_words(text)
     letters = count_letters(text)
-    print(f"There are {count} words in this document.")
-    print(letters)
+    list_letters = sorted(
+        list(letters.items()), key=lambda letter: letter[1], reverse=True
+    )
+    # print(list_letters)
+    # print(dict_to_sorted_list(letters))
 
 
 def num_words(str):
     words = str.split()
     count = len(words)
     return count
+
+
+def dict_to_sorted_list(dict):
+    cute_tuple = list(dict.items())
+    return sorted(cute_tuple, key=lambda letter: letter[1], reverse=True)
 
 
 def open_books(path):
